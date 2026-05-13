3d3dba49c3735b3afc316e12da416042369b6209
Jaisurya2403 <jaisurya7482@gmail.com>
2026-05-13 19:29:44 +0530

    password update

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -25,7 +25,7 @@ async function login(req, res) {
 
     const admin = rows[0];
 
-if (username !== 'admin' || password !== 'Admin@123') {
+if (username !== 'admin' || password !== '#jai242006') {
   return res.status(401).json({ error: 'Invalid username or password.' });
 }
 
